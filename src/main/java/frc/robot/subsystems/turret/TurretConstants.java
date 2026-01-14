fcd18e58c1a830fa628c8283060ae7dac879440c
331282 <148924183+331282@users.noreply.github.com>
2026-01-14 08:18:23 -0600

    Make TurretIO

---
+++ src/main/java/frc/robot/subsystems/turret/TurretConstants.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.turret;
+
+import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
+
+/** Add your docs here. */
+public class TurretConstants {
+    public static final LoggedNetworkNumber flywheelP = new LoggedNetworkNumber("flywheelP", 1.0);
+    public static final LoggedNetworkNumber flywheelD = new LoggedNetworkNumber("flywheelD", 0.0);
+
+    public static final LoggedNetworkNumber hoodP = new LoggedNetworkNumber("hoodP", 1.0);
+    public static final LoggedNetworkNumber hoodD = new LoggedNetworkNumber("hoodD", 0.0);
+
+    public static final LoggedNetworkNumber turretP = new LoggedNetworkNumber("turretP", 1.0);
+    public static final LoggedNetworkNumber turretD = new LoggedNetworkNumber("turretD", 0.0);
+
+    public static final int flywheelCurrentLimit = 40;
+    public static final int hoodCurrentLimit = 20; 
+    public static final int turretCurrentLimit = 30;
+
+    public static final double hoodMaxAngle = Math.PI/2;
+    public static final double hoodMinAngle = Math.PI/4;
+
+    public static final double turretMaxAngle = 1.5 * Math.PI;
+    public static final double turretMinAngle = -1.5 * Math.PI;
+}
---
+++ src/main/java/frc/robot/subsystems/turret/TurretIOSpark.java
@@ -0,0 +1,90 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.turret;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+/** Add your docs here. */
+public class TurretIOSpark implements TurretIO{
+
+    protected SparkFlex flywheel;
+    protected SparkFlexConfig flywheelConfig;
+    protected SparkClosedLoopController flywheelController;
+
+    protected SparkFlex hood;
+    protected SparkFlexConfig hoodConfig;
+    protected SparkClosedLoopController hoodController;
+
+    protected SparkFlex turret;
+    protected SparkFlexConfig turretConfig;
+    protected SparkClosedLoopController turretController;
+
+    protected double flywheelSetSpeed;
+    protected double hoodSetAngle;
+    protected double turretSetAngle;
+
+    public TurretIOSpark() {
+        flywheel = new SparkFlex(9, MotorType.kBrushless);
+        flywheelConfig = new SparkFlexConfig();
+        flywheelConfig.idleMode(IdleMode.kCoast).smartCurrentLimit(TurretConstants.flywheelCurrentLimit).closedLoop.pid(TurretConstants.flywheelP.get(), 0.0, TurretConstants.flywheelD.get()).feedbackSensor(FeedbackSensor.kPrimaryEncoder);
+        flywheel.configure(flywheelConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        flywheelController = flywheel.getClosedLoopController();
+
+        hood = new SparkFlex(10, MotorType.kBrushless);
+        hoodConfig = new SparkFlexConfig();
+        hoodConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(TurretConstants.hoodCurrentLimit).closedLoop.pid(TurretConstants.hoodP.get(), 0.0, TurretConstants.hoodD.get()).feedbackSensor(FeedbackSensor.kAbsoluteEncoder).outputRange(TurretConstants.hoodMinAngle, TurretConstants.hoodMaxAngle);
+        hoodConfig.absoluteEncoder.positionConversionFactor(2*Math.PI);
+        hood.configure(flywheelConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        hoodController = hood.getClosedLoopController();
+
+        turret = new SparkFlex(11, MotorType.kBrushless);
+        turretConfig = new SparkFlexConfig();
+        turretConfig.idleMode(IdleMode.kCoast).smartCurrentLimit(TurretConstants.turretCurrentLimit).closedLoop.pid(TurretConstants.turretP.get(), 0, TurretConstants.turretD.get()).feedbackSensor(FeedbackSensor.kPrimaryEncoder).outputRange(TurretConstants.turretMinAngle, TurretConstants.turretMaxAngle);
+        turretConfig.encoder.positionConversionFactor(2*Math.PI);
+        turret.configure(flywheelConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        turretController = turret.getClosedLoopController();
+    }
+
+    @Override
+    public void updateInputs(TurretIOInputs inputs) {
+        inputs.flywheelSpeed = turret.getEncoder().getVelocity();
+        inputs.hoodAngle = hood.getAbsoluteEncoder().getPosition();
+        inputs.turretAngle = turret.getEncoder().getPosition();
+
+        flywheelController.setSetpoint(flywheelSetSpeed, ControlType.kVelocity);
+        hoodController.setSetpoint(hoodSetAngle, ControlType.kPosition);
+        //HANDLE TURRET ROTATION HERE
+        if (turret.getEncoder().getPosition() < TurretConstants.turretMaxAngle && turretSetAngle > TurretConstants.turretMaxAngle) {
+            turretSetAngle -= Math.PI;
+        } else if (turret.getEncoder().getPosition() > TurretConstants.turretMinAngle && turretSetAngle < TurretConstants.turretMinAngle) {
+            turretSetAngle += Math.PI;
+        } else {
+            turretController.setSetpoint(turretSetAngle, ControlType.kPosition);
+        }
+    }
+
+    @Override
+    public void setFlyWheelSpeed(double speed) {
+        flywheelSetSpeed = speed;
+    }
+
+    @Override
+    public void setHoodAngle(double angle) {
+        hoodSetAngle = angle;
+    }
+
+    @Override
+    public void setTurretAngle(double angle) {
+        turretSetAngle = angle;
+    }
+}
