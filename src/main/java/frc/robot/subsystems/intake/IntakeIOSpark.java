d0d01d5e0ab40fba7dd93bc4c1ed2cf9ed28b062
Tim <233451477+timothyscholl@users.noreply.github.com>
2026-01-23 20:55:39 -0600

    Started work on IntakeIOSpark

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSpark.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSpark.java
@@ -4,5 +4,41 @@
 
 package frc.robot.subsystems.intake;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.sim.SparkFlexExternalEncoderSim;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+
 /** Add your docs here. */
-public class IntakeIOSpark implements IntakeIO {}
+public class IntakeIOSpark implements IntakeIO {
+
+    protected SparkFlex arm;
+    private SparkFlexConfig armConfig;
+    private SparkClosedLoopController armController;
+
+    protected SparkFlex intakeWheels;
+    private SparkFlexConfig intakeWheelConfig;
+    private SparkClosedLoopController intakeWheelController;
+
+    public IntakeIOSpark() {
+        intakeWheels = new SparkFlex(12, MotorType.kBrushless);
+        intakeWheelConfig = new SparkFlexConfig();
+
+        intakeWheelController = intakeWheels.getClosedLoopController();
+
+        intakeWheelConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(40);
+        intakeWheelConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(1, 0, 0);
+        intakeWheels.configure(intakeWheelConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+        arm = new SparkFlex(13, MotorType.kBrushless);
+        armConfig = new SparkFlexConfig();
+
+        armController = arm.getClosedLoopController();
+    }
+}
